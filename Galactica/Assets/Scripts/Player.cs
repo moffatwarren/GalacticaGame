8d8f0ad7b53a9665c9c65dcb501def4d40c14f95
Warren <moffatwarren@gmail.com>
2018-12-29 13:23:59 -0800

    Made player movement

---
+++ Galactica/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] float moveSpeed = 12f;
+	[SerializeField] float padding = 0.5f;
+
+	float xMin;
+	float xMax;
+	float yMin;
+	float yMax;
+
+	// Use this for initialization
+	void Start()
+	{
+		SetUpMoveBoundaries();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		MovePlayer();
+	}
+
+	private void SetUpMoveBoundaries()
+	{
+		Camera gameCamera = Camera.main;
+		xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+		xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+		yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+		yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+	}
+
+	private void MovePlayer()
+	{
+		var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime;
+		var deltaY = Input.GetAxis("Vertical") * Time.deltaTime;
+		var newXpos = Mathf.Clamp(transform.position.x + deltaX  * moveSpeed, xMin, xMax);
+		var newYpos = Mathf.Clamp(transform.position.y + deltaY * moveSpeed, yMin, yMax);
+		transform.position = new Vector2(newXpos, newYpos);
+	}
+}
