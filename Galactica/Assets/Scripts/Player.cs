d5e07ccb266c5523e5901f5c19fdad88d9fea3a9
Warren <moffatwarren@gmail.com>
2018-12-29 14:09:55 -0800

    Made player be able to shoot

--- Galactica/Assets/Scripts/Player.cs
+++ Galactica/Assets/Scripts/Player.cs
@@ -7,11 +7,16 @@ public class Player : MonoBehaviour
 {
 	[SerializeField] float moveSpeed = 12f;
 	[SerializeField] float padding = 0.5f;
+	[SerializeField] GameObject laserPrefab;
+	[SerializeField] float laserVelocity = 15f;
+	[SerializeField] float laserShotDelay = 0.5f;
 
 	float xMin;
 	float xMax;
 	float yMin;
 	float yMax;
+
+	Coroutine firingCoroutine;
 
 	// Use this for initialization
 	void Start()
@@ -23,6 +28,29 @@ public class Player : MonoBehaviour
 	void Update()
 	{
 		MovePlayer();
+		Fire();
+	}
+
+	private void Fire()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			firingCoroutine = StartCoroutine(FireContinously());
+		}
+		if (Input.GetButtonUp("Fire1"))
+		{
+			StopCoroutine(firingCoroutine);
+		}
+	}
+
+	IEnumerator FireContinously()
+	{
+		while (true)
+		{
+			GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+			laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserVelocity);
+			yield return new WaitForSeconds(laserShotDelay);
+		}
 	}
 
 	private void SetUpMoveBoundaries()
